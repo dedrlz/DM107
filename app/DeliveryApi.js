58e80bad88686f0731eefb78dbfef77df9453b52
Luiz Felipe Pereira <felipepwned@hotmail.com>
2015-10-27 00:51:40 -0200

    Crud Finalizado

--- app/DeliveryApi.js
+++ app/DeliveryApi.js
@@ -47,4 +47,113 @@ router.get('/:id', function (req,res){
     });
 });
 
+router.get('/:id', function (req,res){
+   
+    req.getConnection(function(err,conn){
+
+        if (err) return next("Cannot Connect");
+
+        var query = conn.query('SELECT * FROM entregas where idEntrega = ?',[req.params.id],function(err,rows){
+
+            if(err){
+                console.log(err);
+                return next("Mysql error, check your query");
+            }
+	    res.json(rows);
+
+         });
+
+    });
+});
+
+router.post('/', function(req,res) {
+
+    //get data
+    var data = {
+        idEntrega:req.body.idEntrega,
+        idPedido:req.body.idPedido,
+        idCliente:req.body.idCliente,
+        nomeRecebedor:req.body.nomeRecebedor,
+        cpfRecebedor:req.body.cpfRecebedor,
+        recebedorcomprador:req.body.recebedorcomprador,
+        datahoraEntrega:req.body.datahoraEntrega,
+        localizacao:req.body.localizacao
+     };
+
+    //inserting into mysql
+    req.getConnection(function (err, conn){
+
+        if (err) return next("Cannot Connect");
+
+        var query = conn.query("INSERT INTO entregas set ? ",data, function(err, rows){
+
+           if(err){
+                console.log(err);
+                return next("Mysql error, check your query");
+           }
+
+          res.sendStatus(200);
+
+        });
+
+     });
+});
+
+router.put('/:id', function(req,res) {
+    var idEntrega = req.params.id;
+
+    //get data
+    var data = {
+        idEntrega:req.body.idEntrega,
+        idPedido:req.body.idPedido,
+        idCliente:req.body.idCliente,
+        nomeRecebedor:req.body.nomeRecebedor,
+        cpfRecebedor:req.body.cpfRecebedor,
+        recebedorcomprador:req.body.recebedorcomprador,
+        datahoraEntrega:req.body.datahoraEntrega,
+        localizacao:req.body.localizacao
+     };
+
+    //inserting into mysql
+    req.getConnection(function (err, conn){
+
+        if (err) return next("Cannot Connect");
+
+        var query = conn.query("UPDATE entregas set ? WHERE idEntrega = ? ",[data,idEntrega], function(err, rows){
+
+           if(err){
+                console.log(err);
+                return next("Mysql error, check your query");
+           }
+
+          res.sendStatus(200);aa
+
+        });
+
+     });
+
+});
+   
+router.delete('/:id', function(req,res){
+
+    var idEntrega = req.params.id;
+
+     req.getConnection(function (err, conn) {
+
+        if (err) return next("Cannot Connect");
+
+        var query = conn.query("DELETE FROM entregas  WHERE idEntrega = ? ",[idEntrega], function(err, rows){
+
+             if(err){
+                console.log(err);
+                return next("Mysql error, check your query");
+             }
+
+             res.sendStatus(200);
+
+        });
+     });
+});
+
+
 module.exports = router;
